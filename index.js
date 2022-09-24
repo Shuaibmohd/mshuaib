d45a4f5703f077683382de15e0d71c88d98f7355
sheebukhan62 <sheebukhan62@gmail.com>
2022-09-25 01:04:07 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const path = require('path')
+
+const app = express();
+
+// Set the static folde
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+// app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, 'public', 'html', 'index.html'))
+// })
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+
